c7f26ccc25de8d833ac4f4358b483531b1931e84
shadowmoonezios <agushifelde@hotmail.com>
2025-09-06 22:04:21 +0300

    Update MongoDB connection string to use environment variable for database URI.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/tourenplaner', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/tourenplaner', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => console.log('Datenbank verbunden'))
   .catch(err => console.error(err));
 
